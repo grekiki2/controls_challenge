fab67c152bf2a73490d9f19ca31a36121995717e
grekiki2 <gregor1234567890@gmail.com>
2024-05-12 15:11:30 +0200

    pid-baseline

--- controllers.py
+++ controllers.py
@@ -12,8 +12,23 @@ class SimpleController(BaseController):
   def update(self, target_lataccel, current_lataccel, state):
     return (target_lataccel - current_lataccel) * 0.3
 
+class PIDController(BaseController):
+  def __init__(self):
+    self.kp = 0.05
+    self.ki = 0.1
+    self.kd = 0.01
+    self.prev_error = 0
+    self.integral = 0
+
+  def update(self, target_lataccel, current_lataccel, state):
+    error = target_lataccel - current_lataccel
+    self.integral += error
+    derivative = error - self.prev_error
+    self.prev_error = error
+    return self.kp * error + self.ki * self.integral + self.kd * derivative
 
 CONTROLLERS = {
   'open': OpenController,
   'simple': SimpleController,
+  'pid': PIDController
 }
